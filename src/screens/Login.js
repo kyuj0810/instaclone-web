d1b8bce69cd0cff235b5deb976b430ad97661ef0
jin <kyuj0810@gmail.com>
2023-07-21 14:51:18 +0900

    9.8 Themes on Styled Components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,36 +1,22 @@
 import { styled } from 'styled-components';
-import { isLoggedInVar } from '../apollo';
+import { darkModeVar, isLoggedInVar } from '../apollo';
 import { useState } from 'react';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
 const Title = styled.h1`
-  color: ${(props) => (props.potato ? 'palevioletred' : 'beige')};
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  ${(props) =>
-    props.potato
-      ? `
-          font-size: 60px;
-        `
-      : `
-          text-decoration: underline;
-        `}
+  color: ${(props) => props.theme.fontColor};
 `;
 
 const Container = styled.div`
-  background-color: tomato;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
-const TogglePotato = styled.button`
-  color: red;
-`;
 function Login() {
-  const [potato, setPotato] = useState(false);
-  const togglePotato = () => setPotato((current) => !current);
   return (
     <Container>
-      <Title potato={potato}>Login</Title>
-      <TogglePotato onClick={togglePotato}>TogglePotato</TogglePotato>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>To dark</button>
+      <button onClick={() => darkModeVar(false)}>To light</button>
     </Container>
   );
 }
