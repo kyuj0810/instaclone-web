cd5e7c43a097ab89bde2fae1f7912d5701f522d2
jin <kyuj0810@gmail.com>
2023-07-21 13:47:56 +0900

    9.6 Reactive Variables

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { useReactiveVar } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import NotFound from './screens/NotFound';
-import { useState } from 'react';
+import { isLoggedInVar } from './apollo';
 
 function App() {
-  const [isLoggedIn, setIsLoggendIn] = useState(false);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
 
   return (
     <div>
       <Router>
         <Switch>
           <Route path="/" exact>
-            {isLoggedIn ? (
-              <Home setIsLoggendIn={setIsLoggendIn} />
-            ) : (
-              <Login setIsLoggendIn={setIsLoggendIn} />
-            )}
+            {isLoggedIn ? <Home /> : <Login />}
           </Route>
           <Route>
             <NotFound />
@@ -38,5 +30,7 @@ export default App;
  * Switch : 한 번에 딱 하나의 route만 render시켜줌.
  *  exact : url이 정확히 맞는지 확인.
  *browser router와 hash router차이점 : hash outer가 deploy하기 훨씬 쉬움.
- *redirect시키면, 사용자는 가끔 그 경로가 맞는지 틀렸는지 모를 수 있음. reditect 보단 notfound로 선호.
+ *redirect(Router): redirect시키면, 사용자는 가끔 그 경로가 맞는지 틀렸는지 모를 수 있음. reditect 보단 notfound로 선호.
+
+ Reactive variables (Apollo client포함)
  */
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,10 @@
-function Home({ setIsLoggendIn }) {
+import { isLoggedInVar } from '../apollo';
+
+function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => setIsLoggendIn(false)}>Log out now!</button>
+      <button onClick={() => isLoggedInVar(false)}>Log out now!</button>
     </div>
   );
 }
