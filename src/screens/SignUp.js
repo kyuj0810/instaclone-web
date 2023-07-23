d0e7279949bda860a27b8f65436b59c531f48763
jin <kyuj0810@gmail.com>
2023-07-23 20:15:24 +0900

    11.4 Sign Up UI

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,51 @@
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AuthLayout from '../components/auth/AuthLayout';
+import BottomBox from '../components/auth/BottomBox';
+import Button from '../components/auth/Button';
+import FormBox from '../components/auth/FormBox';
+import Input from '../components/auth/Input';
+import Separator from '../components/auth/Separator';
+import routes from '../routes';
+import { styled } from 'styled-components';
+import { FatLink } from '../components/shared';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Subtitle = styled(FatLink)`
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
 function SignUp() {
-  return null;
+  return (
+    <AuthLayout>
+      <FormBox>
+        <div>
+          <HeaderContainer>
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
+            <Subtitle>
+              Sign up to see photos and videos from your friends.
+            </Subtitle>
+          </HeaderContainer>
+        </div>
+        <form>
+          <Input type="text" placeholder="Email" />
+          <Input type="text" placeholder="Full Name" />
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Log up" />
+        </form>
+      </FormBox>
+
+      <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
+    </AuthLayout>
+  );
 }
 
 export default SignUp;
