4ba3f9817e56af9e7b63a373072cf760ff35e9c4
jin <kyuj0810@gmail.com>
2023-07-23 20:03:29 +0900

    11.3 Shared Components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { darkModeVar, isLoggedInVar } from './apollo';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, darkTheme, lightTheme } from './styles';
 import SignUp from './screens/SignUp';
+import routes from './routes';
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
@@ -18,11 +19,11 @@ function App() {
       <div>
         <Router>
           <Switch>
-            <Route path="/" exact>
+            <Route path={routes.home} exact>
               {isLoggedIn ? <Home /> : <Login />}
             </Route>
             {!isLoggedIn ? (
-              <Route path="/sigh-up">
+              <Route path={routes.signUp}>
                 <SignUp />
               </Route>
             ) : null}
