7fa796f14cca6d8bd977d97119da013a985c551c
jin <kyuj0810@gmail.com>
2023-07-28 16:44:32 +0900

    12.5 Photo Component part Two

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,15 @@
 import { gql, useQuery } from '@apollo/client';
-import { logUserOut } from '../apollo';
+
 import { styled } from 'styled-components';
 import Avatar from '../components/Avatar';
 import { FatText } from '../components/shared';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from '@fortawesome/free-regular-svg-icons';
 
 const FEED_QUERY = gql`
   query seeFeed {
@@ -27,16 +34,45 @@ const PhotoContainer = styled.div`
   background-color: white;
   border: 1px solid ${(props) => props.theme.borderColor};
   margin-bottom: 20px;
+  max-width: 612px;
 `;
 
 const PhotoHeader = styled.div`
-  padding: 5px 10px;
+  padding: 15px;
   display: flex;
   align-items: center;
 `;
 
 const Username = styled(FatText)`
   margin-left: 5px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 10px;
+  display: block;
 `;
 
 function Home() {
@@ -48,9 +84,31 @@ function Home() {
       {data?.seeFeed?.map((photo) => (
         <PhotoContainer key={photo.id}>
           <PhotoHeader>
-            <Avatar url={photo.user.avatar} />
+            <Avatar lg url={photo.user.avatar} />
             <Username>{photo.user.username}</Username>
           </PhotoHeader>
+          <PhotoFile src={photo.file} />
+          <PhotoData>
+            <PhotoActions>
+              <div>
+                <PhotoAction>
+                  <FontAwesomeIcon size={'2x'} icon={faHeart} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={'2x'} icon={faComment} />
+                </PhotoAction>
+                <PhotoAction>
+                  <FontAwesomeIcon size={'2x'} icon={faPaperPlane} />
+                </PhotoAction>
+              </div>
+              <div>
+                <FontAwesomeIcon size={'2x'} icon={faBookmark} />
+              </div>
+            </PhotoActions>
+            <Likes>
+              {photo.likes === 1 ? '1 like' : `${photo.likes} likes`}
+            </Likes>
+          </PhotoData>
         </PhotoContainer>
       ))}
     </div>
